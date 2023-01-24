efeece178714ae62b118cb0dd4febf133dec2302
Nick Sieger <nick@nicksieger.com>
2023-01-23 20:56:49 -0600

    Add /opt/homebrew paths for Homebrew/Apple silicon

--- ext/image_science/extconf.rb
+++ ext/image_science/extconf.rb
@@ -1,7 +1,7 @@
 require 'mkmf'
 
-include_path = '/usr/local/include:/usr/include:/opt/local/include:/opt/include'
-lib_path = '/usr/local/lib:/usr/lib:/opt/local/lib:/opt/lib'
+include_path = '/opt/homebrew/include:/usr/local/include:/usr/include:/opt/local/include:/opt/include'
+lib_path = '/opt/homebrew/lib:/usr/local/lib:/usr/lib:/opt/local/lib:/opt/lib'
 
 append_cflags(%w[-Wno-parentheses -Wno-shorten-64-to-32 -Wno-implicit-function-declaration -Wno-compound-token-split-by-macro])
 
